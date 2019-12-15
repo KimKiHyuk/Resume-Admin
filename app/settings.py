5c2d416a68267e6ff65f7a14480e4bddff7a7441
key kim <keyhyuk.kim@gmail.com>
2019-12-15 23:41:10 +0900

    Update setting.py

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resume_manager'
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resume',
+        'USER': secrets_base['DATABASE_ID'],
+        'PASSWORD': secrets_base['DATABASE_PASSWORD'],
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
