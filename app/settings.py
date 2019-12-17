39f971795094309c8b3cb4388a1213bdd1cc770f
key kim <keyhyuk.kim@gmail.com>
2019-12-17 22:06:40 +0900

    update allowed host

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = secrets_base['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['resume-admin.herokuapp.com']
 
 
 # Application definition
