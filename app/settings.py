34cfc5a9337c46cc0df74f99fbafc1a1b084d56e
key kim <keyhyuk.kim@gmail.com>
2020-02-26 19:47:39 +0900

    Add static files

--- app/settings.py
+++ app/settings.py
@@ -15,7 +15,9 @@ import json
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 SECRETS_DIR = os.path.join(BASE_DIR, '.secrets')
 SECRETS_BASE = os.path.join(SECRETS_DIR, 'key.json')
 secrets_base = json.loads(open(SECRETS_BASE, 'rt').read())
@@ -129,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
