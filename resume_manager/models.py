e9fb752ef50eb712391ef37697dca75e1a8eadae
key kim <keyhyuk.kim@gmail.com>
2020-02-27 23:11:09 +0900

    Update project model

--- resume_manager/models.py
+++ resume_manager/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.postgres.fields import ArrayField
+
 # Create your models here.
 
+
 class AboutMe(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
     greet = models.TextField()
-    job = models.CharField(max_length = 20)
+    job = models.CharField(max_length=20)
     profile_image = models.URLField()
     introduce = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = '자기소개'
+        verbose_name_plural = "자기소개"
 
     def __str__(self):
         return self.name
+
 
 class Career(models.Model):
     company = models.CharField(max_length=20)
@@ -26,10 +29,11 @@ class Career(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = '커리어'
+        verbose_name_plural = "커리어"
 
     def __str__(self):
         return self.company
+
 
 class Education(models.Model):
     insititute = models.CharField(max_length=40)
@@ -40,36 +44,40 @@ class Education(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = '교육기관'
+        verbose_name_plural = "교육기관"
 
     def __str__(self):
         return self.insititute
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=20)
     image = models.URLField()
     proficiency = models.IntegerField()
     hasgTags = ArrayField(
-        models.CharField(max_length=15, blank=True, default=''), blank=True, default=list
+        models.CharField(max_length=15, blank=True, default=""),
+        blank=True,
+        default=list,
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = '기술스택'
+        verbose_name_plural = "기술스택"
 
     def __str__(self):
         return self.name
 
+
 class Project(models.Model):
     category = models.CharField(max_length=20)
     title = models.CharField(max_length=30)
-    subtitie = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
     color = models.CharField(max_length=30)
     github = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name_plural = '프로젝트'
+        verbose_name_plural = "프로젝트"
 
     def __str__(self):
         return self.title
